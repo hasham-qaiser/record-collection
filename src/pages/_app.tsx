ece1e4d42140a8f463de3c2f4f38a5b8c6338861
hasham-qaiser <117246204+hasham-qaiser@users.noreply.github.com>
2024-01-22 19:02:56 -0800

    geist added, need to figure out features that are missing font change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
 import { ThemeProvider } from "@/components/ui/theme-provier";
 import "@/styles/globals.css";
+import localFont from "next/font/local";
 import type { AppProps } from "next/app";
+
+const myFont = localFont({ src: "./geist-mono.woff2" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <main className={myFont.className}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
   );
 }
